866082a6f82a6fd86a1122192ee46044e19d0ce0
taegeun <djqtek0525@naver.com>
2023-01-17 16:55:38 +0900

    [feat] : 부모 번호로 조회하는 기능
    ParentRepository에 findByPhoneNum 추가

--- src/main/java/com/project/myacademy/domain/parent/ParentRepository.java
+++ src/main/java/com/project/myacademy/domain/parent/ParentRepository.java
@@ -2,5 +2,8 @@ package com.project.myacademy.domain.parent;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParentRepository extends JpaRepository<Parent, Long> {
+    Optional<Parent> findByPhoneNum(String phoneNum);
 }
