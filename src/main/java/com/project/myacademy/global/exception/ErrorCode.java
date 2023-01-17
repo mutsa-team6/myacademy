349c235af5d292556158706a10f8204230be5965
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-01-17 13:08:34 +0900

    [refactor] : 에러코드 부분 변경

--- src/main/java/com/project/myacademy/global/exception/ErrorCode.java
+++ src/main/java/com/project/myacademy/global/exception/ErrorCode.java
@@ -8,15 +8,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    DUPLICATED_ACADEMY_NAME(HttpStatus.CONFLICT, "이미 존재하는 학원입니다."),
-    DUPLICATED_EMPLOYEE_NAME(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
-    DUPLICATED_STUDENT_NAME(HttpStatus.CONFLICT, "이미 존재하는 학생입니다."),
-    DUPLICATED_PARENT_NAME(HttpStatus.CONFLICT, "이미 등록된 부모입니다."),
-    DUPLICATED_LECTURE_NAME(HttpStatus.CONFLICT, "이미 존재하는 수업입니다."),
-    DUPLICATED_TEACHER_NAME(HttpStatus.CONFLICT, "이미 존재하는 강사입니다."),
-    DUPLICATED_PAYMENT_NAME(HttpStatus.CONFLICT, "이미 결제된 내역입니다."),
+    DUPLICATED_ACADEMY(HttpStatus.CONFLICT, "이미 존재하는 학원입니다."),
+    DUPLICATED_EMPLOYEE(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
+    DUPLICATED_STUDENT(HttpStatus.CONFLICT, "이미 존재하는 학생입니다."),
+    DUPLICATED_PARENT(HttpStatus.CONFLICT, "이미 등록된 부모입니다."),
+    DUPLICATED_LECTURE(HttpStatus.CONFLICT, "이미 존재하는 수업입니다."),
+    DUPLICATED_TEACHER(HttpStatus.CONFLICT, "이미 존재하는 강사입니다."),
+    DUPLICATED_PAYMENT(HttpStatus.CONFLICT, "이미 결제된 내역입니다."),
 
-    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 계정을 찾을 수 없습니다."),
     ACADEMY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 학원을 찾을 수 없습니다."),
     PARENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 부모를 찾을 수 없습니다."),
     LECTURE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 수업을 찾을 수 없습니다."),
@@ -29,7 +29,7 @@ public enum ErrorCode {
     TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
 
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러가 발생했습니다."),
     INVALID_VALUE(HttpStatus.BAD_REQUEST, "요청이 이상합니다."),
     PAYMENT_REQUIRED(HttpStatus.PAYMENT_REQUIRED, "결제 정보가 필요합니다.");
 
