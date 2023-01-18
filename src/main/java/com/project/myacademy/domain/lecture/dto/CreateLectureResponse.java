85e1871bdba22828282d8a45d1e4c98069fd5089
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-01-18 14:15:07 +0900

    [merge] : 7 feat/lecture (#15)
    * [feat] : 강좌 등록,수정,삭제,리스트 조회 기능 구현
    
    * [style] : AcademyRepository 클래스 위치 변경에 따른 import 변경

---
+++ src/main/java/com/project/myacademy/domain/lecture/dto/CreateLectureResponse.java
@@ -0,0 +1,20 @@
+package com.project.myacademy.domain.lecture.dto;
+
+import com.project.myacademy.domain.lecture.Lecture;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class CreateLectureResponse {
+
+    private Long lectureId;
+    private String message;
+
+    public static CreateLectureResponse of(Lecture lecture) {
+        return new CreateLectureResponse(lecture.getId(), "강좌 등록 완료");
+    }
+}
