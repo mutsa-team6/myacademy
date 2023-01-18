18fb1dd83296dde18dc8f4c619b40892bb4422a9
taegeun <djqtek0525@naver.com>
2023-01-18 13:55:40 +0900

    [feat] : 특이사항 전체 조회기능

--- src/main/java/com/project/myacademy/domain/uniqueness/UniquenessRestController.java
+++ src/main/java/com/project/myacademy/domain/uniqueness/UniquenessRestController.java
@@ -2,14 +2,15 @@ package com.project.myacademy.domain.uniqueness;
 
 import com.project.myacademy.domain.uniqueness.dto.CreateUniquenessRequest;
 import com.project.myacademy.domain.uniqueness.dto.CreateUniquenessResponse;
+import com.project.myacademy.domain.uniqueness.dto.ReadAllUniquenessResponse;
 import com.project.myacademy.global.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,13 +21,22 @@ public class UniquenessRestController {
     private final UniquenessService uniquenessService;
 
     /**
-     *
      * @param studentId 특이사항의 대상이 되는 학생 Id
-     * @param request 특이사항의 요청시 받는 request Dto
+     * @param request   특이사항의 요청시 받는 request Dto
      */
-    @PostMapping("students/{studentId}/uniquness")
+    @PostMapping("students/{studentId}/uniqueness")
     public ResponseEntity<Response<CreateUniquenessResponse>> create(@PathVariable Long studentId, CreateUniquenessRequest request) {
-        CreateUniquenessResponse response = uniquenessService.createUniqueness(studentId,request);
+        CreateUniquenessResponse response = uniquenessService.createUniqueness(studentId, request);
         return ResponseEntity.ok().body(Response.success(response));
+    }
+
+    /**
+     * @param studentId 특이사항의 대상이 되는 학생 Id
+     */
+    @GetMapping("students/{studentId}/uniqueness")
+    public ResponseEntity<Response<Page<ReadAllUniquenessResponse>>> readAll(@PathVariable Long studentId) {
+        PageRequest pageable = PageRequest.of(0, 20, Sort.by("id").descending());
+        Page<ReadAllUniquenessResponse> responses = uniquenessService.readAllUniqueness(studentId, pageable);
+        return ResponseEntity.ok().body(Response.success(responses));
     }
 }
--- src/main/java/com/project/myacademy/domain/uniqueness/UniquenessService.java
+++ src/main/java/com/project/myacademy/domain/uniqueness/UniquenessService.java
@@ -2,13 +2,15 @@ package com.project.myacademy.domain.uniqueness;
 
 import com.project.myacademy.domain.student.Student;
 import com.project.myacademy.domain.student.StudentRepository;
-import com.project.myacademy.domain.student.StudentService;
 import com.project.myacademy.domain.uniqueness.dto.CreateUniquenessRequest;
 import com.project.myacademy.domain.uniqueness.dto.CreateUniquenessResponse;
+import com.project.myacademy.domain.uniqueness.dto.ReadAllUniquenessResponse;
 import com.project.myacademy.global.exception.AppException;
 import com.project.myacademy.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +23,8 @@ public class UniquenessService {
     private final StudentRepository studentRepository;
 
     /**
-     *
      * @param studentId 특이사항의 대상이 되는 학생 Id
-     * @param request 특이사항의 요청시 받는 request Dto
+     * @param request   특이사항의 요청시 받는 request Dto
      */
     @Transactional
     public CreateUniquenessResponse createUniqueness(Long studentId, CreateUniquenessRequest request) {
@@ -31,8 +32,20 @@ public class UniquenessService {
         Student student = studentRepository.findById(studentId)
                 .orElseThrow(() -> new AppException(ErrorCode.STUDENT_NOT_FOUND));
 
-        Uniqueness savedUniqueness = uniquenessRepository.save(Uniqueness.toUniqueness(request,student));
+        Uniqueness savedUniqueness = uniquenessRepository.save(Uniqueness.toUniqueness(request, student));
 
         return CreateUniquenessResponse.of(savedUniqueness);
+    }
+
+    /**
+     * @param studentId 특이사항의 대상이 되는 학생 Id
+     * @param pageable  20개씩 id순서대로(최신순대로)
+     */
+    public Page<ReadAllUniquenessResponse> readAllUniqueness(Long studentId, PageRequest pageable) {
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new AppException(ErrorCode.STUDENT_NOT_FOUND));
+
+        return uniquenessRepository.findAllByStudent(student, pageable).map(uniqueness -> ReadAllUniquenessResponse.of(uniqueness));
     }
 }
