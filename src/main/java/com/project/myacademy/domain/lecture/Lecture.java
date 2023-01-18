5c8dc123d21e1b714fc057cf78632f085d772c92
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-01-18 13:46:36 +0900

    [feat] : 강좌 등록,수정,삭제,리스트 조회 기능 구현

--- src/main/java/com/project/myacademy/domain/lecture/Lecture.java
+++ src/main/java/com/project/myacademy/domain/lecture/Lecture.java
@@ -1,12 +1,16 @@
 package com.project.myacademy.domain.lecture;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.myacademy.domain.BaseEntity;
+import com.project.myacademy.domain.lecture.dto.CreateLectureRequest;
+import com.project.myacademy.domain.lecture.dto.UpdateLectureRequest;
 import com.project.myacademy.domain.teacher.Teacher;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -41,14 +45,40 @@ public class Lecture extends BaseEntity {
     private String LectureTime;
 
     @Column(name = "start_date")
-    private LocalDateTime startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
+    private LocalDate startDate;
 
     @Column(name = "finish_date")
-    private LocalDateTime finishDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
+    private LocalDate finishDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "teacher_id")
     private Teacher teacher;
 
+    public static Lecture addLecture(CreateLectureRequest request, Teacher teacher) {
+        return Lecture.builder()
+                .name(request.getLectureName())
+                .price(request.getLecturePrice())
+                .minimumCapacity(request.getMinimumCapacity())
+                .maximumCapacity(request.getMaximumCapacity())
+                .LectureDay(request.getLectureDay())
+                .LectureTime(request.getLectureTime())
+                .startDate(request.getStartDate())
+                .finishDate(request.getFinishDate())
+                .teacher(teacher)
+                .build();
+    }
+
+    public void updateLecture(UpdateLectureRequest request) {
+        this.name = request.getLectureName();
+        this.price = request.getLecturePrice();
+        this.minimumCapacity = request.getMinimumCapacity();
+        this.maximumCapacity = request.getMaximumCapacity();
+        this.LectureDay = request.getLectureDay();
+        this.LectureTime = request.getLectureTime();
+        this.startDate = request.getStartDate();
+        this.finishDate = request.getFinishDate();
+    }
 
 }
