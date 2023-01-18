f3592c37f2c4f75da5ec2f6f3036dbdc0033063c
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-01-18 15:41:27 +0900

    [merge] : 6 feat/teacher (#20)
    * [test] : Teacher클래스 필드 변경에 따른 더미 데이터 변경
    
    * [feat] : 강사 등록,수정,삭제 기능 추가
    
    * [feat] : 강사의 강좌 페이징 기능 초기 구현

--- src/main/java/com/project/myacademy/domain/teacher/TeacherRepository.java
+++ src/main/java/com/project/myacademy/domain/teacher/TeacherRepository.java
@@ -1,6 +1,14 @@
 package com.project.myacademy.domain.teacher;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+    @Query(value = "SELECT t from Teacher t WHERE t.name = :name",
+            countQuery = "SELECT COUNT(t) FROM Teacher t WHERE t.name = :name")
+    Page<Teacher> findByName(String name, Pageable pageable);
+
 }
