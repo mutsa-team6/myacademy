b544626869d49f29a7e14b411c52a0f93378b6c4
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-01-21 17:23:28 +0900

    [feat] : 소셜 로그인, 로그아웃 기능 추가
    1. 소셜 로그인 시, 세션에 사용자 이름 저장, 쿠키에 jwt 토큰 HttpOnly, Secure 설정 후 저장
    
    2. 로그아웃 시, 쿠키에 저장되어 있는 토큰 내용을 deleted 로 변경, 세션 삭제 기능 추가

---
+++ src/main/java/com/project/myacademy/controller/IndexController.java
@@ -0,0 +1,73 @@
+package com.project.myacademy.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.CookieGenerator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model){
+
+        //회원 이름 표시
+        HttpSession session = request.getSession(true);
+
+        if (session.getAttribute("name") != null) {
+            String loginUserName = (String)session.getAttribute("name");
+            log.info("세션에 저장된 실명 : [{}]",loginUserName);
+            model.addAttribute("name", loginUserName);
+        }
+        return "index";
+    }
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/oauthFail")
+    public String oauthFail(){
+        return "oauthFail";
+    }
+
+    @GetMapping("/logoutEmployee")
+    public String logout(HttpServletRequest request,HttpServletResponse response){
+        CookieGenerator cookieGenerator = new CookieGenerator();
+        cookieGenerator.setCookieName("token");
+        cookieGenerator.addCookie(response,"deleted");
+        cookieGenerator.setCookieMaxAge(0);
+
+        HttpSession session = request.getSession();
+        session.removeAttribute("name");
+        return "index";
+    }
+
+    @GetMapping("/oauth2/redirect")
+    public String login(@RequestParam String token, HttpServletResponse response){
+        CookieGenerator cookieGenerator = new CookieGenerator();
+        cookieGenerator.setCookieName("token");
+        cookieGenerator.setCookieHttpOnly(true);
+        cookieGenerator.setCookieSecure(true);
+        cookieGenerator.addCookie(response,token);
+        cookieGenerator.setCookieMaxAge(60*60);//1시간
+        log.info("🍪 쿠키에 저장한 토큰 {}",token);
+        return "redirect:/";
+    }
+
+
+}
