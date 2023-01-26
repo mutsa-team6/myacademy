669dc64750a7af87bec2fac1378c9a76d8547e77
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-01-26 11:36:41 +0900

    [fix] : 배포 서버 템플릿 엔진 오류 해결

--- src/main/java/com/project/myacademy/controller/EmployeeController.java
+++ src/main/java/com/project/myacademy/controller/EmployeeController.java
@@ -18,24 +18,24 @@ public class EmployeeController {
 
     @GetMapping("/join")
     public String join() {
-        return "/employee/join";
+        return "employee/join";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "/employee/login";
+        return "employee/login";
     }
 
     @GetMapping("/find/account")
     public String findAccount() {
 
-        return "/employee/find";
+        return "employee/find";
     }
 
     @GetMapping("/find/password")
     public String findPassword() {
 
-        return "/employee/findPassword";
+        return "employee/findPassword";
     }
 
     @GetMapping("/oauthFail")
