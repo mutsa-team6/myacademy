bbe09889e9de23999c22292d6e70dedafec495bf
taegeun <djqtek0525@naver.com>
2023-01-27 23:38:51 +0900

    [feat] : 학생특이사항 관리 권한 추가
    학생특이사항은 ADMIN, STAFF 만 관리가능함

--- src/main/java/com/project/myacademy/domain/uniqueness/UniquenessService.java
+++ src/main/java/com/project/myacademy/domain/uniqueness/UniquenessService.java
@@ -39,6 +39,10 @@ public class UniquenessService {
         Academy academy = validateAcademy(academyId);
         //account 유효검사
         Employee employee = validateAcademyEmployee(account, academy);
+        // 학생 특이사항을 수정할 수 있는 권한인지 확인(강사만 불가능)
+        if(Employee.isTeacherAuthority(employee)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
+        }
         //student Id에 해당하는 학생이 존재하는지 확인
         Student student = validateStudent(studentId);
 
@@ -58,6 +62,10 @@ public class UniquenessService {
         Academy academy = validateAcademy(academyId);
         //account 유효검사
         Employee employee = validateAcademyEmployee(account, academy);
+        // 학생 특이사항을 수정할 수 있는 권한인지 확인(강사만 불가능)
+        if(Employee.isTeacherAuthority(employee)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
+        }
         //student Id에 해당하는 학생이 존재하는지 확인
         Student student = validateStudent(studentId);
 
@@ -77,6 +85,10 @@ public class UniquenessService {
         Academy academy = validateAcademy(academyId);
         //account 유효검사
         Employee employee = validateAcademyEmployee(account, academy);
+        // 학생 특이사항을 수정할 수 있는 권한인지 확인(강사만 불가능)
+        if(Employee.isTeacherAuthority(employee)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
+        }
         //student Id에 해당하는 학생이 존재하는지 확인
         Student student = validateStudent(studentId);
         //uniquenessId에 등록된 특이사항이 있는지 확인
@@ -99,6 +111,10 @@ public class UniquenessService {
         Academy academy = validateAcademy(academyId);
         //account 유효검사
         Employee employee = validateAcademyEmployee(account, academy);
+        // 학생 특이사항을 수정할 수 있는 권한인지 확인(강사만 불가능)
+        if(Employee.isTeacherAuthority(employee)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
+        }
         //student Id에 해당하는 학생이 존재하는지 확인
         Student student = validateStudent(studentId);
         //uniquenessId에 등록된 특이사항이 있는지 확인
