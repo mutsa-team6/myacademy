f6bc7906dec0eb84a0e225067d9f65fde75d8713
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-01-28 21:55:54 +0900

    [build] : Jacoco 적용 1. build.gradle 플러그인 추가 2. 커버러지 결과, 검증단계에서 제외할 클래스들 추가 3. Jacoco 적용 전 작성한 테스트코드들 주석 처리

--- src/test/java/com/project/myacademy/MyacademyApplicationTests.java
+++ src/test/java/com/project/myacademy/MyacademyApplicationTests.java
@@ -54,108 +54,107 @@ class MyacademyApplicationTests {
 	@Autowired
 	WaitinglistRepository waitinglistRepository;
 
-	@Test
-	@DisplayName("가짜 데이터 넣기")
-	@Transactional
-	@Rollback(value = false)
-	void addExampleData() {
-		Academy academy = Academy.builder()
-				.name("Academy name")
-				.businessRegistrationNumber("123-456")
-				.address("Academy address")
-				.owner("Academy owner")
-				.phoneNum("Academy phone number")
-				.build();
-
-		Academy academy2 = Academy.builder()
-				.name("Academy name")
-				.businessRegistrationNumber("123-789")
-				.address("Academy address")
-				.owner("Academy owner")
-				.phoneNum("Academy phone number")
-				.build();
-
-		Employee employee = Employee.builder()
-				.academy(academy)
-				.account("Employee account")
-				.password("Employee password")
-				.address("Employee address")
-				.email("Employee email")
-				.phoneNum("Employee phone number")
-				.name("Employee name")
-				.employeeRole(EmployeeRole.ROLE_USER)
-				.build();
-
-		Teacher teacher = Teacher.builder()
-				.name("Teacher name")
-				.subject("Teacher subject")
-				.employee(employee)
-				.build();
-
-		Lecture lecture = Lecture.builder()
-				.teacher(teacher)
-				.name("lecture name")
-				.LectureDay("Lecture day")
-				.LectureTime("Lecture Time")
-				.price(300000)
-				.maximumCapacity(30)
-				.minimumCapacity(5)
-				.startDate(LocalDate.now())
-				.finishDate(LocalDate.now().plusDays(10))
-				.currentEnrollmentNumber(1)
-				.build();
-
-		Parent parent =Parent.builder()
-				.address("Parent address")
-				.phoneNum("Parent phone number")
-				.parentRecognizedCode(0)
-				.name("Parent name")
-				.build();
-
-		Student student = Student.builder()
-				.address("Student address")
-				.school("Student school")
-				.parent(parent)
-				.name("Student name")
-				.email("Student email")
-				.phoneNum("Student phone number")
-				.build();
-
-		Payment payment = Payment.builder()
-				.student(student)
-				.lecture(lecture)
-				.employee(employee)
-				.build();
-
-		Enrollment enrollment = Enrollment.builder()
-				.student(student)
-				.lecture(lecture)
-				.memo("Student Lecture memo")
-				.build();
-
-		Uniqueness uniqueness = Uniqueness.builder()
-				.body("Uniqueness body")
-				.student(student)
-				.build();
-
-		Waitinglist waitingList = Waitinglist.builder()
-				.lecture(lecture)
-				.student(student)
-				.memo("memo")
-				.build();
-
-
-		academyRepository.save(academy);
-		academyRepository.save(academy2);
-		employeeRepository.save(employee);
-		lectureRepository.save(lecture);
-		parentRepository.save(parent);
-		paymentRepository.save(payment);
-		enrollmentRepository.save(enrollment);
-		studentRepository.save(student);
-		teacherRepository.save(teacher);
-		uniquenessRepository.save(uniqueness);
-		waitinglistRepository.save(waitingList);
-	}
+//	@Test
+//	@DisplayName("가짜 데이터 넣기")
+//	@Transactional
+//	@Rollback(value = false)
+//	void addExampleData() {
+//		Academy academy = Academy.builder()
+//				.name("Academy name")
+//				.businessRegistrationNumber("123-456")
+//				.address("Academy address")
+//				.owner("Academy owner")
+//				.phoneNum("Academy phone number")
+//				.build();
+//
+//		Academy academy2 = Academy.builder()
+//				.name("Academy name")
+//				.businessRegistrationNumber("123-789")
+//				.address("Academy address")
+//				.owner("Academy owner")
+//				.phoneNum("Academy phone number")
+//				.build();
+//
+//		Employee employee = Employee.builder()
+//				.academy(academy)
+//				.account("Employee account")
+//				.password("Employee password")
+//				.address("Employee address")
+//				.email("Employee email")
+//				.phoneNum("Employee phone number")
+//				.name("Employee name")
+//				.employeeRole(EmployeeRole.ROLE_USER)
+//				.build();
+//
+//		Teacher teacher = Teacher.builder()
+//				.name("Teacher name")
+//				.subject("Teacher subject")
+//				.employee(employee)
+//				.build();
+//
+//		Lecture lecture = Lecture.builder()
+//				.teacher(teacher)
+//				.name("lecture name")
+//				.LectureDay("Lecture day")
+//				.LectureTime("Lecture Time")
+//				.price(300000)
+//				.maximumCapacity(30)
+//				.minimumCapacity(5)
+//				.startDate(LocalDate.now())
+//				.finishDate(LocalDate.now().plusDays(10))
+//				.currentEnrollmentNumber(1)
+//				.build();
+//
+//		Parent parent =Parent.builder()
+//				.address("Parent address")
+//				.phoneNum("Parent phone number")
+//				.parentRecognizedCode(0)
+//				.name("Parent name")
+//				.build();
+//
+//		Student student = Student.builder()
+//				.school("Student school")
+//				.parent(parent)
+//				.name("Student name")
+//				.email("Student email")
+//				.phoneNum("Student phone number")
+//				.build();
+//
+//		Payment payment = Payment.builder()
+//				.student(student)
+//				.lecture(lecture)
+//				.employee(employee)
+//				.build();
+//
+//		Enrollment enrollment = Enrollment.builder()
+//				.student(student)
+//				.lecture(lecture)
+//				.memo("Student Lecture memo")
+//				.build();
+//
+//		Uniqueness uniqueness = Uniqueness.builder()
+//				.body("Uniqueness body")
+//				.student(student)
+//				.build();
+//
+//		Waitinglist waitingList = Waitinglist.builder()
+//				.lecture(lecture)
+//				.student(student)
+//				.memo("memo")
+//				.build();
+//
+//
+//		academyRepository.save(academy);
+//		academyRepository.save(academy2);
+//		employeeRepository.save(employee);
+//		lectureRepository.save(lecture);
+//		parentRepository.save(parent);
+//		paymentRepository.save(payment);
+//		enrollmentRepository.save(enrollment);
+//		studentRepository.save(student);
+//		teacherRepository.save(teacher);
+//		uniquenessRepository.save(uniqueness);
+//		waitinglistRepository.save(waitingList);
+//	}
 
 }
