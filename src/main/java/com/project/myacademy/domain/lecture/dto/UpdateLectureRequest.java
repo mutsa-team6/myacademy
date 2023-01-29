bb604761afa00d84897af31d559c99e5e0a8f8ee
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-01-29 21:02:52 +0900

    [fix] : Lecture 테이블 Employee 테이블과 연관관계 매핑 1. 강좌-직원 N:1 연관관계 매핑 2. 강좌 등록, 수정, 삭제 endpoint 수정 3. 그에 따른 DTO 필드 수정

--- src/main/java/com/project/myacademy/domain/lecture/dto/UpdateLectureRequest.java
+++ src/main/java/com/project/myacademy/domain/lecture/dto/UpdateLectureRequest.java
@@ -1,6 +1,5 @@
 package com.project.myacademy.domain.lecture.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +15,6 @@ public class UpdateLectureRequest {
 
     private String lectureName;
     private Integer lecturePrice;
-//    private String teacherName;
     private Integer minimumCapacity;
     private Integer maximumCapacity;
     private String lectureDay;
