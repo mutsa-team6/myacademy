ee42bab12d69a82be5594042d0ee75804766c0c0
taegeun <djqtek0525@naver.com>
2023-01-30 21:13:42 +0900

    [refactor] : Academy Service 학원 update 주석처리

--- src/main/java/com/project/myacademy/domain/academy/AcademyService.java
+++ src/main/java/com/project/myacademy/domain/academy/AcademyService.java
@@ -58,48 +58,48 @@ public class AcademyService {
 //        return savedAcademy.toAcademyDto();
 //    }
 
-    /**
-     * 학원 정보 수정
-     *
-     * @param academyId
-     * @param reqeust
-     * @param name
-     * @return AcademyDto
-     */
-    @Transactional
-    public AcademyDto updateAcademy(Long academyId, UpdateAcademyReqeust reqeust, String name) {
-
-        // 인증 확인
-        log.info("인증정보로 저장소에서 계정정보를 확인합니다.");
-        Employee employee = employeeRepository.findByName(name)
-                .orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
-        log.info("계정정보가 확인되었습니다.");
-
-        // academyId로 학원정보  확인
-        log.info("academyId로 저장소에서 학원정보를 조회합니다.");
-        Academy academy = academyRepository.findById(academyId)
-                .orElseThrow(() -> new AppException(ErrorCode.ACADEMY_NOT_FOUND));
-        log.info("저장소에서 학원정보가 조회되었습니다.");
-
-        // 권한 확인
-        log.info("학원정보의 소유자와 인증정보로 권한을 확인합니다.");
-        if (!academy.getOwner().equals(name) && !employee.getEmployeeRole().equals(EmployeeRole.ROLE_ADMIN)) {
-            throw new AppException(ErrorCode.INVALID_PERMISSION);
-        }
-        log.info("권한이 확인되었습니다.");
-
-        // 학원 정보 수정
-        log.info("학원정보를 수정하겠습니다.");
-        academy.update(reqeust);
-        log.info("학원정보를 수정하였습니다.");
-
-        // 학원 정보 저장
-        log.info("수정된 학원정보를 저장하겠습니다.");
-        Academy updatedAcademy = academyRepository.save(academy);
-        log.info("수정된 학원정보를 저장하겠습니다.");
-
-        return updatedAcademy.toAcademyDto();
-    }
+//    /**
+//     * 학원 정보 수정
+//     *
+//     * @param academyId
+//     * @param reqeust
+//     * @param name
+//     * @return AcademyDto
+//     */
+//    @Transactional
+//    public AcademyDto updateAcademy(Long academyId, UpdateAcademyReqeust reqeust, String name) {
+//
+//        // 인증 확인
+//        log.info("인증정보로 저장소에서 계정정보를 확인합니다.");
+//        Employee employee = employeeRepository.findByName(name)
+//                .orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+//        log.info("계정정보가 확인되었습니다.");
+//
+//        // academyId로 학원정보  확인
+//        log.info("academyId로 저장소에서 학원정보를 조회합니다.");
+//        Academy academy = academyRepository.findById(academyId)
+//                .orElseThrow(() -> new AppException(ErrorCode.ACADEMY_NOT_FOUND));
+//        log.info("저장소에서 학원정보가 조회되었습니다.");
+//
+//        // 권한 확인
+//        log.info("학원정보의 소유자와 인증정보로 권한을 확인합니다.");
+//        if (!academy.getOwner().equals(name) && !employee.getEmployeeRole().equals(EmployeeRole.ROLE_ADMIN)) {
+//            throw new AppException(ErrorCode.INVALID_PERMISSION);
+//        }
+//        log.info("권한이 확인되었습니다.");
+//
+//        // 학원 정보 수정
+//        log.info("학원정보를 수정하겠습니다.");
+//        academy.update(reqeust);
+//        log.info("학원정보를 수정하였습니다.");
+//
+//        // 학원 정보 저장
+//        log.info("수정된 학원정보를 저장하겠습니다.");
+//        Academy updatedAcademy = academyRepository.save(academy);
+//        log.info("수정된 학원정보를 저장하겠습니다.");
+//
+//        return updatedAcademy.toAcademyDto();
+//    }
 
     /**
      * 학원 삭제
