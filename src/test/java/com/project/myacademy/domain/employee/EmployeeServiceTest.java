be068dcc4189d29f58876e9325b7ae4e7706773f
taegeun <djqtek0525@naver.com>
2023-02-01 14:55:32 +0900

    [feat] : 강사 등록 service Test
    USER,STAFF,ADMIN 따로 만듦

---
+++ src/test/java/com/project/myacademy/domain/employee/EmployeeServiceTest.java
@@ -0,0 +1,216 @@
+package com.project.myacademy.domain.employee;
+
+import com.project.myacademy.domain.academy.Academy;
+import com.project.myacademy.domain.academy.AcademyRepository;
+import com.project.myacademy.domain.employee.dto.CreateEmployeeRequest;
+import com.project.myacademy.domain.employee.dto.CreateEmployeeResponse;
+import com.project.myacademy.global.exception.AppException;
+import com.project.myacademy.global.exception.ErrorCode;
+import com.project.myacademy.global.util.EmailUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class EmployeeServiceTest {
+    private EmployeeService employeeService;
+    BCryptPasswordEncoder bCryptPasswordEncoder = Mockito.mock(BCryptPasswordEncoder.class);
+    EmailUtil emailUtil;
+
+    EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
+
+    AcademyRepository academyRepository = Mockito.mock(AcademyRepository.class);
+
+    @InjectMocks
+    private Academy academy;
+    private Employee employeeADMIN, employeeSTAFF, employeeUSER;
+    private Pageable pageable;
+    @Value("${jwt.token.secret}")
+    String secretKey;
+
+    @BeforeEach
+    void setUp() {
+        employeeService = new EmployeeService(employeeRepository, academyRepository, bCryptPasswordEncoder, emailUtil);
+        academy = Academy.builder().id(1L).name("학원").owner("원장").build();
+        employeeADMIN = Employee.builder().id(1L).name("원장").account("admin").phoneNum("010-0000-0000").email("employeeADMIN@gmail.com").academy(academy).employeeRole(EmployeeRole.ROLE_ADMIN).build();
+        employeeSTAFF = Employee.builder().id(2L).name("직원").account("staff").phoneNum("010-0000-0001").email("employeeSTAFF@gmail.com").academy(academy).employeeRole(EmployeeRole.ROLE_STAFF).build();
+        employeeUSER = Employee.builder().id(3L).name("강사").account("user").phoneNum("010-0000-0002").email("employeeUSER@gmail.com").academy(academy).employeeRole(EmployeeRole.ROLE_USER).build();
+        pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "id");
+    }
+
+    @Nested
+    @DisplayName("원장 등록")
+    class CreateEmployeeADMIN {
+
+        @Test
+        @DisplayName("원장 등록 성공")
+        void create_employee_admin_success() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "1", "과목");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestADMIN.getPassword());
+            given(employeeRepository.save(any())).willReturn(employeeADMIN);
+
+            CreateEmployeeResponse response = employeeService.createEmployee(requestADMIN, academy.getId());
+
+            assertThat(response.getName().equals("원장"));
+            assertThat(response.getAccount().equals("admin"));
+        }
+
+        @Test
+        @DisplayName("원장 등록 실패5 - 계정 중복")
+        void create_employee_admin_fail5() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "1", "과목");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.of(employeeADMIN));
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(requestADMIN, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.DUPLICATED_ACCOUNT));
+        }
+
+        @Test
+        @DisplayName("원장 등록 실패4 - 이메일 중복")
+        void create_employee_admin_fail4() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "1", "과목");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.of(employeeADMIN));
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(requestADMIN, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.DUPLICATED_EMAIL));
+        }
+
+        @Test
+        @DisplayName("원장 등록 실패3 - 계정이 admin이지만 대표자명과 일치하지 않는경우")
+        void create_employee_admin_fail3() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장아님", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "1", "코딩");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestADMIN.getPassword());
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(requestADMIN, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.NOT_MATCH_OWNER));
+        }
+
+        @Test
+        @DisplayName("원장 등록 실패2 - 직원유형 입력안함")
+        void create_employee_admin_fail2() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "0", "코딩");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestADMIN.getPassword());
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(requestADMIN, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.EMPTY_SUBJECT_FORBIDDEN));
+        }
+
+        @Test
+        @DisplayName("원장 등록 실패1 - 과목입력 안함")
+        void create_employee_admin_fail1() {
+            CreateEmployeeRequest requestADMIN = new CreateEmployeeRequest("원장", "원장주소", "010-0000-0001", "원장@gmail.com", "admin", "password", "1", "");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestADMIN.getPassword());
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(requestADMIN, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.EMPTY_EMPLOYEE_TYPE));
+        }
+    }
+
+    @Nested
+    @DisplayName("직원 등록")
+    class CreateEmployeeSTAFF {
+
+        @Test
+        @DisplayName("직원 등록 성공")
+        void create_employee_staff_success() {
+            CreateEmployeeRequest requestSTAFF = new CreateEmployeeRequest("직원", "직원주소", "010-0000-0002", "직원@gmail.com", "staff", "password", "", "");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestSTAFF.getPassword());
+            given(employeeRepository.save(any())).willReturn(employeeSTAFF);
+
+            CreateEmployeeResponse response = employeeService.createEmployee(requestSTAFF, academy.getId());
+
+            assertThat(response.getName().equals("강사"));
+            assertThat(response.getAccount().equals("staff"));
+        }
+    }
+
+    @Nested
+    @DisplayName("강사 등록")
+    class CreateEmployeeUSER {
+
+        @Test
+        @DisplayName("강사 등록 성공")
+        void create_employee_user_success() {
+            CreateEmployeeRequest requestUSER = new CreateEmployeeRequest("강사", "강사주소", "010-0000-0003", "강사@gmail.com", "user", "password", "USER", "코딩");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(requestUSER.getPassword());
+            given(employeeRepository.save(any())).willReturn(employeeUSER);
+
+            CreateEmployeeResponse response = employeeService.createEmployee(requestUSER, academy.getId());
+
+            assertThat(response.getName().equals("강사"));
+            assertThat(response.getAccount().equals("user"));
+        }
+
+        @Test
+        @DisplayName("강사 등록 실패")
+        void create_employee_user_fail1() {
+            CreateEmployeeRequest emptySubRequestUSER = new CreateEmployeeRequest("강사", "강사주소", "010-0000-0003", "강사@gmail.com", "user", "password", "USER", "");
+
+            given(academyRepository.findById(any())).willReturn(Optional.of(academy));
+            given(employeeRepository.findByAccountAndAcademy(any(), any())).willReturn(Optional.empty());
+            given(employeeRepository.findByNameAndEmail(any(), any())).willReturn(Optional.empty());
+            given(bCryptPasswordEncoder.encode(any())).willReturn(emptySubRequestUSER.getPassword());
+            given(employeeRepository.save(any())).willReturn(employeeUSER);
+
+            AppException appException = assertThrows(AppException.class,
+                    () -> employeeService.createEmployee(emptySubRequestUSER, academy.getId()));
+
+            assertThat(appException.getErrorCode().equals(ErrorCode.EMPTY_SUBJECT_FORBIDDEN));
+        }
+    }
+
+}
