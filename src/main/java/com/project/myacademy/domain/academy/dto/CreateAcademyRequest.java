6b07e626441825b683ed711f8af41c5badd105a3
taegeun <djqtek0525@naver.com>
2023-02-12 00:03:59 +0900

    [refactor] : 안쓰는 import 제거

--- src/main/java/com/project/myacademy/domain/academy/dto/CreateAcademyRequest.java
+++ src/main/java/com/project/myacademy/domain/academy/dto/CreateAcademyRequest.java
@@ -1,6 +1,5 @@
 package com.project.myacademy.domain.academy.dto;
 
-import com.project.myacademy.domain.academy.Academy;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
